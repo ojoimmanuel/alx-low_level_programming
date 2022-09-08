1fa8235c684f4f728d169e342024d641a12137b4
Emmanuel ojo <ojoimmanuel@gmail.com>
2022-09-08 12:04:51 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-	char Mystr[] = "Programming is like building a multilingual puzzle";
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Mystr);
 
-	return 0;
+	return (0);
 }
