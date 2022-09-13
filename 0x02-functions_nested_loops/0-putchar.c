513dd8b00c9c99f6b08d9f4d0a2f4e13d5dfd52f
Emmanuel ojo <ojoimmanuel@gmail.com>
2022-09-13 10:16:39 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,16 +5,14 @@
  *
  * Return: 0 (Success)
  */
-
 int main(void)
 {
 	char text[8] = "_putchar";
 	int i = 0;
-
-	for (i=0; i<8; i++)
+	for (i = 0; i < 8; i++)
 	{
 		_putchar(text[i]);
 	}
 	_putchar('\n');
-        return (0);
+	return (0);
 }
