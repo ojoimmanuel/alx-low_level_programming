217ec38419ac88b78103f31455f4075da400e6b8
Emmanuel ojo <ojoimmanuel@gmail.com>
2022-09-13 13:49:47 -0700

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,9 +12,9 @@ int print_last_digit(int n)
 	int last_digit = n % 10;
 
 	if (last_digit < 0)
-		return (last_digit * -1);
-	else
-		return (last_digit);
+		last_digit = last_digit * -1;
+	
+	_putchar(last_digit + '0');
+
 	return (last_digit);
-	_putchar('\n');
 }
