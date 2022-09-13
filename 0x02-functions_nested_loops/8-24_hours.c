a0cc258f95a540a67c7f21e68f97c9bb430fcd61
Emmanuel ojo <ojoimmanuel@gmail.com>
2022-09-13 14:34:06 -0700

    update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,10 +12,10 @@ void jack_bauer(void)
 	int n = 0;
 	while (n< 2400)
 	{
-		_putchar(n/1000 + '0');
-		_putchar(n/100 + '0');
+		_putchar((n/1000)%10 + '0');
+		_putchar((n/100)%10 + '0');
 		_putchar(':');
-		_putchar(n/10 + '0');
+		_putchar((n/10)%10 + '0');
 		_putchar(n%10 + '0');
 		_putchar('\n');
 		n++;
