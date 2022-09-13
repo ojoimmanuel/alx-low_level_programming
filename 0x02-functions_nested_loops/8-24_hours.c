ed65f2d4ab6f7f1b61026e6855ef6cdf6df5d9ce
Emmanuel ojo <ojoimmanuel@gmail.com>
2022-09-13 14:45:18 -0700

    update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 
 /**
  * jack_bauer - prints minutes from 00:00 to 23:59
- * 
+ *
  *
  * Return: 0 (Success)
  */
@@ -10,14 +10,20 @@
 void jack_bauer(void)
 {
 	int n = 0;
-	while (n< 2400)
+	int tenth = 0;
+
+	while (n < 2400)
 	{
-		_putchar((n/1000)%10 + '0');
-		_putchar((n/100)%10 + '0');
-		_putchar(':');
-		_putchar((n/10)%10 + '0');
-		_putchar(n%10 + '0');
-		_putchar('\n');
+		tenth = (n / 10) % 10;
+		if (tenth < 6)
+		{
+			_putchar((n / 1000) % 10 + '0');
+			_putchar((n / 100) % 10 + '0');
+			_putchar(':');
+			_putchar((n / 10) % 10 + '0');
+			_putchar(n % 10 + '0');
+			_putchar('\n');
+		}
 		n++;
 	}
 	return;
