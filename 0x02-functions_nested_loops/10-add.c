45ef2527ebe00674a5bfb5e4b70b984281a744a3
Emmanuel ojo <ojoimmanuel@gmail.com>
2022-09-13 15:56:03 -0700

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - computes the addition of 2 numbers
+ * @n: n is a variable
+ * @m: m is a variable
+ *
+ * Return: 0 (Success)
+ */
+
+int add(int n, int m)
+{
+	return (n + m);
+	_putchar('\n');
+}
