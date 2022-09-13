8c5d2fa480f5d14c7687f2430718ea78edb432c8
Emmanuel ojo <ojoimmanuel@gmail.com>
2022-09-13 16:21:59 -0700

    update

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints from n to 98
+ * @n: n is a variable
+ *
+ *
+ * Return: 0 (Success)
+ */
+
+void print_to_98(int n)
+{
+	if (n == 98)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if (n < 99)
+	{
+		while (n < 99)
+		{
+			_putchar((n / 10) % 10 + '0');
+			_putchar(n % 10 + '0');
+			if (n < 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n++;
+		}
+	}
+	else
+	{
+		while(n > 98)
+		{
+			_putchar((n / 100) % 10 + '0');
+			_putchar((n / 10) % 10 + '0');
+			_putchar(n % 10 + '0');
+			if (n > 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n--;
+		}
+	}
+}
