6cfb246c6218c5ee94c204c4d2039a5e244ded7c
Emmanuel ojo <ojoimmanuel@gmail.com>
2022-09-13 13:07:50 -0700

    update

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print-sign - prints sign of a number
+ * @n: n is a variable
+ *
+ * Return: 0 (Success)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	_putchar('\n');
+}
