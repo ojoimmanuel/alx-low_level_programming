00b148e87565a753644fa510c41904bccfc1649e
Emmanuel ojo <ojoimmanuel@gmail.com>
2022-09-13 13:39:33 -0700

    update

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of a number
+ * 
+ *
+ * Return: 0 (Success)
+ */
+
+int print_last_digit(int)
+{
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+		return (last_digit * -1);
+	else
+		return (last_digit);
+	_putchar('\n');
+}
