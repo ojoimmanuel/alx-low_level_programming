fddcaeaa191326aca0a79ca64d798981080069e6
Emmanuel ojo <ojoimmanuel@gmail.com>
2022-09-16 15:00:31 -0700

    update

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - product of two numbers
+ * @a: a is first
+ * @b: b is second
+ *
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+}
+	return (a * b);
+}
