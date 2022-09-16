170c337e5f35c2c9c467c510adeea75196d9f636
Emmanuel ojo <ojoimmanuel@gmail.com>
2022-09-16 06:05:35 -0700

    update

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,7 +7,7 @@
  * Return: 1 if upper, 0 is lowercase
  */
 
-int _isupper(char c)
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,4 +4,4 @@
 #include <stdio.h>
 
 int _putchar(char c);
-int _isupper(char c);
+int _isupper(int c);
