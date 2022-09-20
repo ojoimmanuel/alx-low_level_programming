dde1ca2759a7595f10396deefc38c09f902da162
Emmanuel ojo <ojoimmanuel@gmail.com>
2022-09-20 13:56:08 -0700

    update

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - print half of string
+ * @str: string
+ *
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+	int i = 0, j, high;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	high = i;
+	j = high / 2;
+	while (j <= high)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
