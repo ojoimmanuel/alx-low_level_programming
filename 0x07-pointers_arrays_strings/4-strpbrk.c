14be48df5c2afd1f025035526f71eb56e981038e
Emmanuel ojo <ojoimmanuel@gmail.com>
2022-10-02 08:47:32 -0700

    update

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#define NULL 0
+
+/**
+ * _strpbrk - searches string for a set of bytes
+ * @s: given string
+ * @accept: source
+ *
+ * Return: pointer to match bytes in initial segmnt of s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0, j = 0;
+
+	while (*(s + i) != '\0')
+	{
+		while (*(accept + j) != '\0')
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				return (s + i);
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return (NULL);
+}
