fc5052b408f940028d415d35d2c0f248b226551c
Emmanuel ojo <ojoimmanuel@gmail.com>
2022-10-03 00:51:47 -0700

    update

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,7 +13,9 @@ char *_strstr(char *haystack, char *needle)
 	int i = 0, j = 0, start = 0, len = 0;
 
 	while (needle[len] != '\0')
+	{
 		len++;
+	}
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (j = 0; j < len && haystack[i] == needle[j]; j++, i++)
