e416de41ceee8b58beb9fa62eaa7e8d49b0fea12
Emmanuel ojo <ojoimmanuel@gmail.com>
2022-10-04 08:55:16 -0700

    upload

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: string
+ *
+ * Return: pointer
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_puts_recursion((s + 1));
+		_putchar(*(s + 0));
+	}
+}
