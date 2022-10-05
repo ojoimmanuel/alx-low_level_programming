8a3eb970c06f51c4c8fdaafcd4ba4d48ed4c6628
Emmanuel ojo <ojoimmanuel@gmail.com>
2022-10-05 09:50:00 -0700

    update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,17 +10,13 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (j = 0; j < 15; j++)
 	{
-		for (j = 0; j < 15; j++)
+		if (j >= 10)
 		{
-			if (j >= 10)
-			{
-				_putchar('1');
-				_putchar(j % 10 + '0');
-			}
+			_putchar('1');
+			_putchar(j % 10 + '0');
 		}
-			_putchar('\n');
+		_putchar('\n');
 	}
-	return (0);
 }
