f346e6607575a766a095da3b81ad3a431be0e593
Emmanuel ojo <ojoimmanuel@gmail.com>
2022-10-06 15:05:02 -0700

    upload

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ * * is_number - check if num
+ * @s: character to check
+ *
+ * Return: 1 if it's a number, 0 otherwise
+ */
+
+int is_number(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) >= '0' && *(s + i) <= '9')
+		{
+			i++;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+/**
+ * main - prints the sum of positive numbers
+ * @argc: number of arg
+ * @argv: array of arg
+ *
+ * Return: (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum, is_num;
+
+	sum = 0;
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else if (argc > 1)
+	{
+		i = 1;
+		while (i < argc)
+		{
+			is_num = is_number(argv[i]);
+			if (is_num == 1)
+			{
+				sum = sum + atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+			i++;
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
