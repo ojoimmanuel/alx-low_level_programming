2692ac8aa0434975d504eb4efce36f35591341a6
Emmanuel ojo <ojoimmanuel@gmail.com>
2022-10-06 14:29:54 -0700

    upload

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+
+/**
+ * main - prints program name
+ * @argc: number of arguments
+ * @argv: array of arg
+ *
+ * Return: (0)
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
