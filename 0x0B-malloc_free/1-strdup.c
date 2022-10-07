90cb5d8748d6902dae5c7dc15b46e2c21b1b21a6
Emmanuel ojo <ojoimmanuel@gmail.com>
2022-10-07 02:03:06 -0700

    upload

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to newly allocated space in mmry
+ * @str: string
+ *
+ * Return: pointer to new space
+ */
+
+char *_strdup(char *str)
+{
+	int length, i;
+	char *array;
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	length = 0;
+	while (*(str + len) != '\0')
+	{
+		len++;
+	}
+
+	array = malloc(len * sizeof(char) + 1);
+	if (array == NULL)
+		return (NULL);
+
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = str[i];
+	}
+	array[i] = '\0';
+
+	return (array);
+}
