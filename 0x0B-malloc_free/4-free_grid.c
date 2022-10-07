f9c9ef3b435511354c716cb190b20dbfda1c6695
Emmanuel ojo <ojoimmanuel@gmail.com>
2022-10-07 10:48:38 -0700

    upload

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free a grid
+ * @grid: grid given
+ * @height: rows
+ *
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	i = 0;
+	while (i < height)
+	{
+		free(grid[i]);
+		i++;
+	}
+		free(grid);
+	}
+	return (0);
+}
