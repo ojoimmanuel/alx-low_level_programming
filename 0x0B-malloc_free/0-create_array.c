cb28ce2fbc680f1e949d5817faace09cad07ecda
Emmanuel ojo <ojoimmanuel@gmail.com>
2022-10-07 01:43:02 -0700

    upload

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 
 	array = (char*) malloc(sizeof(char) * size);
 	if (array == NULL)
-		retunr (NULL);
+		return (NULL);
 	if (size != 0)
 	{
 		for (i = 0; i < size; i++)
