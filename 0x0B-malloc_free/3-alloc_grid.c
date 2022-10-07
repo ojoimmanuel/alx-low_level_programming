d6c8100c12a7073a07cd8293b5b2d20a8e276c32
Emmanuel ojo <ojoimmanuel@gmail.com>
2022-10-07 10:39:26 -0700

    upload

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a grid
+ * @width: columns
+ * @height: rows
+ *
+ * Return: pointer to grid
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = (int **) malloc(sizeof(int *) * height);
+
+	if (grid == NULL)
+		retun (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			while (i >= 0)
+			{
+				free(grid[i]);
+				i--;
+			}
+			free(grid);
+			return (NULL);
+		}
+	}
+	return (grid);
+}
