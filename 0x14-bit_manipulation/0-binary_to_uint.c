28c74ec299582e79ad1fd3d57437ab1246589775
ojoimmanuel <ojoimmanuel@gmail.com>
2022-10-28 05:35:54 -0700

    upload

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -44,7 +44,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		sum += (b[i] - '0' * pow(2, length - i - 1);
+		sum += (b[i] - '0' * pow(2, len - i - 1);
 	}
 	return (sum);
 
