d87c844c46a898497f8eb6f3f957f7f1cbafc5d0
ojoimmanuel <ojoimmanuel@gmail.com>
2022-10-28 05:21:42 -0700

    upload

---
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * pow - calculate power
+ * @num: given number
+ * @exp: exponential
+ *
+ * Return: result
+ */
+
+int pow(int num, int exp)
+{
+	int i, result;
+
+	result = 1;
+	for (i = 0; i < exp; ++i)
+		num *= base;
+
+	return (result);
+}
+
+/**
+ * binary_to_uint - convertsa binary number to an unsigned int
+ * @b: binary
+ *
+ * Return: unsigned int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int sum;
+	int len, i;
+
+	len = 0;
+	sum = 0;
+	if (b == NULL)
+		return (0);
+	while (b[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		sum += (b[i] - '0' * pow(2, length - i - 1);
+	}
+	return (sum);
+
+}
