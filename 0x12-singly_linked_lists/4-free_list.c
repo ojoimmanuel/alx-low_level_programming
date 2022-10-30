c660bd4738cc654b3e7576ef8ea48a4b20712a5d
ojoimmanuel <ojoimmanuel@gmail.com>
2022-10-30 07:24:04 -0700

    upload

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - fxn to free a list_t list
+ * @head: head of a linked list
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+	
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
