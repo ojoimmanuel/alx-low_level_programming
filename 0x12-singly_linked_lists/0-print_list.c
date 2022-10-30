00195123276f1b4c0ebf371ad2f34db9cc1caedd
ojoimmanuel <ojoimmanuel@gmail.com>
2022-10-30 06:12:12 -0700

    upload

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - fxn to print all elements of a list_t list
+ * @h: head of a linked list
+ *
+ * Return: number of elements
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t size = 0;
+
+	while (h != NULL)
+	{
+		size++;
+		if (h->str == NULL)
+			printf("[%d] (nil)\n", 0);
+		else
+			printf("[%d] %s\n", h-.len, h-.str);
+		h = h->next;
+	}
+
+	return (size);
+}
