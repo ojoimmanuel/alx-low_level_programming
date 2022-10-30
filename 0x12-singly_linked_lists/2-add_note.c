bfdbeba3c6387c0e2d4369a61b29d4d9305bf2c9
ojoimmanuel <ojoimmanuel@gmail.com>
2022-10-30 06:36:51 -0700

    upload

---
+++ 0x12-singly_linked_lists/2-add_note.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * add_node - fxn to add a new node at beginning of list_t list
+ * @head: head of a linked list
+ * @str: string data for node
+ *
+ * Return: number of elements
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int len;
+	char *string;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	if (str == NULL)
+	{
+		string = NULL;
+		len = 0;
+	}
+	else
+	{
+		string = strdup(str);
+		while (str[len] != '\0')
+		{
+			len++;
+		}
+	}
+
+	new->len = len;
+	new->str = string;
+	new->next = *head;
+	*head = new;
+	
+	return (*head);
+}
