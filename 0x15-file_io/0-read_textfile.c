73cb1d649f62b77274df4d8abf48f21067b7f6e0
ojoimmanuel <ojoimmanuel@gmail.com>
2022-10-31 01:27:10 -0700

    upload

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * read_textfile - fxn to read a text file and print to standard output
+ * @filename: name of file
+ * @letters: number of letters
+ *
+ * Return: number of letters if success, 0 otherwise
+ */
+
+ssze_t read_textfile(const char *filename, sixe_t letters)
+{
+	int fd;
+	char *buf;
+
+	ssize_t bytes_read, bytes_written;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters + 1);
+	if (buf == NULL)
+		return (0);
+
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+		return (0);
+
+	buf[letters + 1] = '\0';
+	close(fd);
+
+	bytes_written = write(STDOUT_FILEN), buf, bytes_read);
+	if (bytes_written == -1)
+		return (0);
+
+	free(buf);
+
+	return (bytes_read);
+}
